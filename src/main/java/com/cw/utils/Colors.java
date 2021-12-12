8756a093dc6db86bcc86f43c824c8bf995c65f8e
Wang_xubin <Wang_xubin@gmail.com>
2021-12-12 21:13:12 +0800

    ui design

--- src/main/java/com/cw/utils/Colors.java
+++ src/main/java/com/cw/utils/Colors.java
@@ -2,11 +2,11 @@ package com.cw.utils;
 
 public class Colors
 {
-	public static String DEFAULT = "#F4F4F4";
+	public static String DEFAULT = "#f4f4f4";
 	public static String BACKGROUND = "#FFFFFF";
 	public static String BACKGROUND_LEVEL = "#fff600";
-	public static String SELECT_LEVELPACK = "#00fffc";
-	public static String SELECT_LEVEL = "#fff600";
+	public static String SELECT_LEVELPACK = "#00aeff";
+	public static String SELECT_LEVEL = "#00aeff";
 	public static String BUTTON = "#FFFFFF";
 	public static String BUTTON_LIGHT = "#CCCCCC";
 }
